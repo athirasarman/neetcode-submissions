36d2885e42170bda8eee6c6d6125d80829294afc
Athira <kaithakkottuathira@gmail.com>
2026-06-10 10:08:14 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !==t.length) return false;
+        const charMap = new Map<string, number>();
+        for(const char of s){
+            charMap.set(char, (charMap.get(char) || 0)+1);
+        }
+        for(const char of t){
+            if(!charMap.has(char) || charMap.get(char)===0){
+                return false;
+            }
+            charMap.set(char, charMap.get(char)!-1);
+        }
+        return true;
+    }
+}
